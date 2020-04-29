b07632d90aa7f2f1f1be140f07179113f4ae2fd2
KhoaThy <khoathy@Minhs-iMac.local>
2020-04-28 19:30:11 -0700

    feat: landing page draft

---
+++ js/main.js
@@ -0,0 +1,67 @@
+
+
+$(document).ready(function(){
+    var containerHeight = $('.header').height();
+
+    $(window).scroll(function(){
+        //parallax header
+        var wScroll = $(this).scrollTop();
+        if(wScroll <= containerHeight) {
+            $('.header__middle-1').css({
+                'transform': 'translate(0%, '+ (wScroll/40) +'%)'
+            });
+            $('.header__middle-2').css({
+                'transform': 'translate(0%, '+ (wScroll/4) +'%)'
+            });
+            $('.header__back').css({
+                'transform': 'translate(0%, '+ wScroll/80 +'%)'
+            });
+            $('.header__fore').css({
+                'transform': 'translate(0%, -'+ wScroll/50 +'%)'
+            });
+        }
+       
+    
+        // sticky header 
+        
+        if(wScroll >= ($('.section-about').offset().top - 50)){
+            $('.nav').addClass('nav__sticky');
+        } else {
+            $('.nav').removeClass('nav__sticky');
+        }
+
+
+
+        //decoration animation-left
+        if(wScroll > $('.section-about').offset().top + $(window).height()/20 && wScroll < $('.section-services').offset().top) {
+                $('.decor-left').css({
+                'transform': 'rotate('+(wScroll/20-29) +'deg)'
+            });
+        
+        };
+
+        //decoration animation-right
+        if(wScroll > $('.section-portfolio').offset().top - $(window).height()/3 && wScroll < $('.footer').offset().top) {
+            $('.decor-right').css({
+                'transform': 'rotate('+(-wScroll/30+85) +'deg)'
+            });
+        
+        };
+
+    });
+
+
+    //menu modal
+    $('.menu').click(function(){
+        $('.nav__menu').toggleClass('visible');
+        console.log('visible toggle')
+    });
+    $(document).click(function(event) {
+        //if click on anything except the modal itself or the "open modal" link, close the modal
+        if (!$(event.target).closest(".nav__menu,.menu").length) {
+          $("body").find(".nav__menu").removeClass("visible");
+          console.log('click ouside')
+        }
+    });
+
+});
